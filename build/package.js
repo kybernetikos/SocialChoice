5fc370dabe0965d1e15ce69e95c3ad5dacc69646
kybernetikos <kybernetikos@gmail.com>
2013-10-02 22:03:22 +0100

    call browserify directly instead of build\package

--- build/package.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-var fs = require('fs');
-var Browserify = require('browserify');
-
-var bundle = Browserify();
-bundle.add('..');
-bundle.bundle({
-	standalone: "SocialChoice"
-}).pipe(fs.createWriteStream(path.join(__dirname, "..", "target", "SocialChoice.js")));
